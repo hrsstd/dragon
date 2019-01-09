23c83d0e4bb90fd81be37716e15ca7b4bbe92d84
zhanghuilong <zhanghuilong@corp.netease.com>
2019-01-09 10:37:19 +0800

    第一次 commit

---
+++ src/main/java/com/huilong/cloud/dragon/TestGitPush.java
@@ -0,0 +1,15 @@
+package com.huilong.cloud.dragon;
+
+/**
+ * @author zhanghuilong
+ * @desc test git push reset
+ * @since 2019/01/09
+ */
+public class TestGitPush {
+
+    public static void main(String[] args) {
+        System.out.println("git commit first");
+        System.out.println("git push");
+    }
+
+}
